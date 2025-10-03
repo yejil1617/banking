f6f8ab5ae85032ce88ec6e8d8906e4dbf97b23a1
yejil1617 <90877301+yejil1617@users.noreply.github.com>
2025-10-03 13:36:05 -0500

    some progress before adding shadcn components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 
 const Home = () => {
+  const loggedIn={firstName: 'Yeji'};
   return (
     <section className='home'>
         <div className='home-content'>
             <header className='home-header'>
-                Welcome
+                <HeaderBox 
+                  type="greeting"
+                  title="Welcome"
+                  user={loggedIn?.firstName || 'Guest'}
+                  subtext="Access and manage your account and transactions efficiently."
+                />
+                <TotalBalanceBox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.35}
+                />
             </header>
         </div>
     </section>
---
+++ components/ui/MobileNav.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const MobileNav = ({ user }: MobileNavProps) => {
+  return (
+    <section>
+    </section>
+  );
+};
+
+export default MobileNav;
