e754fe4f33218f5ac5623b94971be8a09f12a6a7
yejil1617 <90877301+yejil1617@users.noreply.github.com>
2025-10-03 13:39:45 -0500

    more updates before shadcn

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,28 @@
+import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
+import { log } from "console";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn={firstName: 'Yeji', lastName: 'Lim'};
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image  src="/icons/logo.svg" width={30} height={30} alt="Horizon Logo"/>
+            <div>
+              <MobileNav user={log}/>
+            </div>
+          </div>
+          {children}
+        </div>
+        
     </main>
   );
 }
